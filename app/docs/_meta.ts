066b86b039a5ba517dc1c95dbf5c10a2a3e1356a
Hwigeon <bb7788@naver.com>
2026-01-16 01:15:28 +0900

    feat: meta 파일 생성

---
+++ app/docs/_meta.ts
@@ -0,0 +1,9 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  index: "Introduction",
+  "getting-started": "Getting Started",
+  components: "Components",
+};
+
+export default meta;
